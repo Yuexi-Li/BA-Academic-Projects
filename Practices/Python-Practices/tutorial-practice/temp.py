8bd256afdfbc85a201e69dceff99030cfb84f027
Yuexi Li <yli8@scu.edu>
2018-07-15 10:55:36 -0700

    Add files

---
+++ Practices/Python-Practices/tutorial-practice/temp.py
@@ -0,0 +1,8 @@
+name='Sara'
+
+def my_print():
+  print("My name is",name)
+  return "??"
+
+if __name__ == '__main__':
+  my_print()
---
+++ Practices/Python-Practices/tutorial-practice/temp1.py
@@ -0,0 +1,5 @@
+import temp as t
+
+# print(temp.my_print())
+
+# print(temp.name)

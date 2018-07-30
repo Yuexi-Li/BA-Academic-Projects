96b1ac411e54bc9e0445deb77376fe17c6c03400
Yuexi Li <yli8@scu.edu>
2018-07-30 15:56:05 -0700

    Push new linkedlist

--- Practices/Python-Practices/tutorial-practice/Linkedlist.py
+++ Practices/Python-Practices/tutorial-practice/Linkedlist.py
@@ -4,91 +4,89 @@ Add three functions to the LinkedList.
 The "insert" function will add an element to a particular
 spot in the list.
 "delete" will delete the first element with that
-particular value."""
-
+particular value.
+Then, use "Test Run" and "Submit" to run the test cases
+at the bottom."""
 import unittest
-#Class Node: create a Node object 
+
 class Node(object):
-  #Node: Value and pointer inside 
-  def __init__(self, value):
-    self.value=value #value in node 
-    # the pointer of that node since its single nodethe pointer points to next is None 
-    self.next= None 
-
+    def __init__(self, value):
+      """The node will contain the value and pointer part.
+      initiate the pointer to next is None."""
+      self.value = value
+      self.next = None
+        
 class LinkedList(object):
-  #Linkedlist: had a head
-  def __init__(self, head=None):
-    self.head=head 
-  
-  def append(self, node):
-    current=self.head
-    if self.head:
-      while current.next:   # while current next is not none 
-        current=current.next
-      current.next=node
-    else:
-      self.head= node 
-  
-  def get_position(self, position):
-    """
-    Get a node at given position. 
-    Assume the first position is '1'.
-    Return "None" if position is not in the list.
-    """
-    if position<=0:
-      return None 
-    if not self.head:
-      return None 
-    index=1
-    current=self.head
-    while current.next!= None:
-      if position == index:
-        return current 
-      current=current.next
-      index+=1
-    if position==index:
-      return current
-    return None 
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, node):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = node
+        else:
+            self.head = node
+            
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        if position <= 0:
+          return None
+        if not self.head:
+          return None
+        index = 1
+        current = self.head
+        while current.next!= None:
+          if position==index:
+            return current
+          current= current.next
+          index+=1        
+        if position==index:
+          return current
+        return None
+          
+        
+    def insert(self, node, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+        current= self.head 
+        if position==1:
+          node.next=current
+          self.head=node
+          return 
+        index=1
+        while current!= None:
+          if position==index+1:
+            node.next=current.next
+            current.next=node
+            return 
+          current= current.next  
+          index+=1  
+        raise IndexError('{}, Out of range'.format(position))
+          
+    
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        current= self.head
+        if current.value== value:
+            self.head=current.next
+            return 
+        while current.next != None: 
+          if current.next.value== value:
+            current.next=current.next.next
+            return 
+          current= current.next
+        
 
-  def insert(self,node,position):
-    """
-    insert a node in a given position. For example, 
-    input position 2 will insert the node between 2nd and 3rd node.
-    The position of the linkedlist starts with 1.
-    If the position is out of the length of linkedlist, 
-    an indexError would be raised.
-    """
-    if position<=0:
-      return None
-    current=self.head
-    #The position is 1 which insert after the node
-    if position==1: 
-      node.next= current
-      self.head=node
-      return 
-    index=1
-    while current!= None:
-      if position==index+1:
-          node.next=current.next
-          current.next=node 
-          return
-      current=current.next 
-      index+=1
-    raise IndexError('{}, Out of range'.format(position))
+        
 
-  def delete(self, value):
-    """Delete the first node with a given value."""
-    current=self.head
-    if current.value==value:
-      self.head=current.next 
-      return 
-    while current.next != None:
-      if current.next.value==value:
-        current.next=current.next.next 
-        return 
-      current=current.next 
-  
 class MyTest(unittest.TestCase):
+  
   def test1(self):
     # Test cases
     # Set up some Nodes
@@ -124,6 +122,6 @@ class MyTest(unittest.TestCase):
     # Should print 3 now
     self.assertEqual(ll.get_position(3).value,3)
 
+
 if __name__ == '__main__':
     unittest.main()
-
---
+++ Practices/Python-Practices/tutorial-practice/coding-challenge.py
@@ -0,0 +1,17 @@
+"""
+Roman to Integer 
+"""
+
+class Solution():
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        I=1
+        V-10
+        x=50
+        L=50
+        C=100
+        D=500
+        M=1000
